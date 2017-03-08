8243bc506dd5994a9a84362dd66c08b58a81cb73
Ic3Venom <Ic3Venom@gmail.com>
2017-03-07 21:00:17 -0800

    Line 48 does not work, unknown reason why.

--- flashcards.py
+++ flashcards.py
@@ -11,48 +11,50 @@
 from random import shuffle
 
 def main():
-    fileName  = raw_input('What is the flashcard file name? (include file type)')
-    fileSplit = raw_input('What character separates the term and definition?')
+    fileName  = str(raw_input('What is the flashcard file name? (include file type)'))
+    fileSplit = str(raw_input('What character separates the term and definition?'))
 
     try:
         #Used to create random card order
         with open(fileName, 'r') as f:
-            length = 0
+            rngLen = 0
             for line in f:
-                length += 1
+                rngLen+= 1
+            f.close()
 
-        random = list(range(length))
-        shuffle(random)
-        #random also carries its own index integer
-        random.insert(1, 1)
+        rng = list(range(rngLen))
+        shuffle(rng)
+        #rng also carries its own index integer
+        rng.insert(0, 1)
     except:
         print 'Unknown file name \'%s\'. Exiting program.' % fileName
         exit(1)
 
     with open(fileName, 'r') as f:
+        #Alternatives to 'while True' require 2+ variables
+            while True:
+                i = 0
+                f.seek(0)
+                for line in f:
+                    if i == rng[rng[0] + 1]:
+                        j = 0
+                        for char in line.split():
+                            if char == fileSplit:
+                                term = ' '.join(line.split()[:j])
+                                definition = ' '.join(line.split()[j+1:])
+                            j += 1
 
-        while True:
-            i = 0
-            for line in f:
-                if i == random[random[0]]:
+                        userInput = str(raw_input(term + ': '))
+                        if userInput.lower() is not definition.lower():
+                            print 'Wrong! It should be %s' % definition
+                            rng[i + 1] = str(definition, fileSplit, term)
 
-                    j = 0
-                    for char in line.split():
-                        if char == fileSplit:
-                            term = ' '.join(line.split()[:j])
-                            definition = ' '.join(line.split()[j+1:])
-                        j += 1
-
-                    userInput = raw_input(term)
-                    if userInput.lower is not definition.lower:
-                        print 'Wrong! It should be %s' % definition
-                        random[i] = definition, fileSplit, term
-
-                    random[0] = int(random[0]) + 1
+                        rng[0] += 1
+                        break
+                    i += 1
+                if rng[0] is rngLen:
                     break
-            if i is length:
-                break
-
+            f.close()
 
 if __name__ == '__main__':
     main()
